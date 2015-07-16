fd7362ddf9f4430393e9e713d4666f9f8242a41e
jinking.xu <jinking.xu@vipshop.com>
2015-07-16 18:31:42 +0800

    modify picture

---
+++ love/js/mylove.js
@@ -0,0 +1,84 @@
+	$(document).ready(
+			function() {
+				$('#fullpage')
+						.fullpage(
+								{
+									'css3' : true,
+									'sectionsColor' : [ '#999', '#4BBFC3',
+											'#7BAABE', '#f90', '#fc9',
+											'#5CDFC3', '#ACAABE', '#f88',
+											'#C91' ],
+									'verticalCentered' : false,
+									'navigation' : true,
+									'navigationTooltips' : [ '我们', '情人节', '生活',
+											'POPO', 'Jiking', '爱情时钟', '一生相伴',
+											'我们结婚吧', '写下祝福' ],
+									'menu' : '#menu',
+									'afterLoad' : function(anchorLink, index){
+										if(index == 9){
+											$(".page1_yun1").hide();
+											$(".page1_yun2").hide();
+											$(".page1_yun3").hide();
+										}else{
+											$(".page1_yun1").show();
+											$(".page1_yun2").show();
+											$(".page1_yun3").show();
+										}
+										
+										if(index != 1){
+											$("#back").show().mouseover(function(){
+												$(this).animate({
+													bottom : "30"
+												}, 200)
+											}).mouseout(function() {
+												$(this).animate({
+													bottom : "20"
+												}, 200)
+											}).stop().animate({
+												borderRadius : "50%",
+												opacity : 1
+											}, 1000).click(function() {
+												$('#fullpage').fullpage.moveTo(1);
+											});	
+																					
+										}else{
+											$("#back").stop().animate({
+												opacity : 0
+											}, 500, function() {
+												$("#back").css({
+													borderRadius : 0
+												}).hide();
+											});										
+										}					
+									}
+								});
+			});
+			
+		setInterval(function() {
+			$(function() {
+				$(".page1_yun1").fadeIn('1000').animate({
+					left : '860'
+				}, 20000, 'linear').animate({
+					left : '0'
+				}, 20000, 'linear')
+		
+			})
+		}, 0);
+		setInterval(function() {
+			$(function() {
+				$(".page1_yun2").fadeIn('1000').animate({
+					right : '750'
+				}, 20000, 'linear').animate({
+					right : '0'
+				}, 20000, 'linear')
+			})
+		}, 0);
+		setInterval(function() {
+			$(function() {
+				$(".page1_yun3").fadeIn('1000').animate({
+					left : '660'
+				}, 15000, 'linear').animate({
+					left : '0'
+				}, 15000, 'linear')
+			})
+		}, 0);	
